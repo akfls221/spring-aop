791b25be38d3d3346dd34e8f79265fc83f775c3a
엄태권 <akfls221@gmail.com>
2022-06-06 02:56:37 +0900

    PointCut 분리

---
+++ src/main/java/taekwon/springaop/order/aop/AspectV2.java
@@ -0,0 +1,24 @@
+package taekwon.springaop.order.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Slf4j
+@Aspect
+public class AspectV2 {
+
+    @Pointcut("execution(* taekwon.springaop.order..*(..))")//pointCut signature
+    private void allOrder() {
+
+    }
+
+
+    @Around("allOrder()")
+    public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable{
+        log.info("[log] {}", joinPoint.getSignature()); // join point 시그니처
+        return joinPoint.proceed();
+    }
+}
